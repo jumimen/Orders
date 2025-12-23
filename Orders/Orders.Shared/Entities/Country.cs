d928d1689f7dc13f48db56a2ba3fc07042a019df
jumimen <jumime@hotmail.com>
2025-12-23 17:37:07 -0500

    First commit with development

--- Orders/Orders.Shared/Entities/Country.cs
+++ Orders/Orders.Shared/Entities/Country.cs
@@ -6,7 +6,7 @@ public class Country
 {
     public int Id { get; set; }
 
-    //[Display("País")]
+    [Display(Name = "País")]
     [MaxLength(80, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
     [Required(ErrorMessage = "El campo {0} es obligatorio.")]
     public string Name { get; set; } = null!;
